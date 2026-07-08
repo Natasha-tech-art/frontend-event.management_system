0904e576d2721f2b520c91d2d5ddfa382b0650af
Natasha-tech-art <natashabolyn4@gmail.com>
2026-07-08 22:40:17 +0300

    Fix backend endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://event-management-platform-jbr3.onrender.com/api/v1'; // Replace with your actual API base URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
